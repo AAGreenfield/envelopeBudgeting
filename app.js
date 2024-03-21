34224211969f960b23230598bd21ef8e12c3eecf
Ayrton Greenfield <ayrtongreenfield@gmail.com>
2024-03-21 15:21:24 +0000

    Set view engine to ejs and created session

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
+const session = require('express-session');
 
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+app.use(session({
+    secret: 'Envelope Secret',
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 app.use((req, res, next) => {
